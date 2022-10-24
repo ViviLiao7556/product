0c8715793edd9cb6ee18094aa4fb1ed29b9793d2
vivi liao <viviliao0506@gmail.com>
2022-10-24 10:11:55 +0800

    add ptoduct.py

---
+++ product.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入價格: ')
+	#products.append(name)
+	#products.append(price)
+	p=[name, price]
+	#p.append(name)
+	#p.append(price)
+	products.append(p)
+
+print(products)
+
